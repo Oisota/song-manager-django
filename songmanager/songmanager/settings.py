2af7a4156d9d4b12127906fe51afeb9fd980a1fd
Derek Morey <derek.o.morey@gmail.com>
2024-02-11 20:57:38 -0500

    Login sorta working, added crispy forms
    - login/logout seems to be working
    - added crispy forms for easy form rendering
    - updated templates

--- songmanager/songmanager/settings.py
+++ songmanager/songmanager/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_htmx',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'core',
 ]
 
@@ -119,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -127,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Crispy Forms
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
