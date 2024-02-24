17bb01a08d83e7b8683203f0c6f11ef96fe592ec
Derek Morey <derek.o.morey@gmail.com>
2024-02-24 14:42:30 -0500

    Song duration input now a multi input field
    The input now has a minutes and seconds number input instead of just the
    total seconds. This will make it easier to input the length of the song.
    
    Still need to fix layout as it's currently just to vertical inputs
    instead of inline.

--- songmanager/core/forms.py
+++ songmanager/core/forms.py
@@ -1,10 +1,11 @@
 """App Forms"""
 
-from django.forms import ModelForm, HiddenInput
+from django.forms import ModelForm, HiddenInput, MultiWidget, NumberInput
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, HTML, Submit
+from crispy_forms.layout import Layout, HTML, Submit, MultiWidgetField, MultiField
+from crispy_forms.bootstrap import InlineField
 
 from core.models import Song, User
 
@@ -19,10 +20,38 @@ class RegistrationForm(UserCreationForm):
         self.helper.add_input(Submit('submit', 'Submit', css_class='btn btn-primary'))
 
 
+class SongDurationWidget(MultiWidget):
+    """Split duration into separate minutes/seconds inputs"""
+    def __init__(self, attrs=None):
+        number_attrs = {'max': 60, 'min': 0}
+        widgets = [
+            NumberInput(attrs=number_attrs),
+            NumberInput(attrs=number_attrs)
+        ]
+        super().__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            m, s = divmod(value, 60)
+            return [m, s]
+        return [0, 0]
+
+    def value_from_datadict(self, data, files, name):
+        """Convert to whole number of seconds"""
+        minutes_str, seconds_str = super().value_from_datadict(data, files, name)
+        minutes = int(minutes_str)
+        seconds = int(seconds_str)
+        return (minutes * 60) + seconds
+
+
+
 class SongForm(ModelForm):
     class Meta:
         model = Song
         fields = ['name', 'duration', 'status']
+        widgets = {
+            'duration': SongDurationWidget()
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -32,6 +61,12 @@ class SongForm(ModelForm):
         self.helper.attrs['hx-target'] = '#song-table-view'
         self.helper.attrs['hx-trigger'] = 'click from:#modal-add-btn'
         self.helper.attrs['hx-on:htmx:after-request'] = 'App.addSongSuccess()'
+
+        self.helper.layout = Layout(
+            'name',
+            MultiWidgetField('duration', attrs={'class': 'mb-1'}),
+            'status'
+        )
 
         if self.instance.id: # song being edited
             self.helper.form_id = f'edit-song-form-{self.instance.id}'
