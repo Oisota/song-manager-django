28f138ce0c301d2e483b7abead5f57fcfb9abd9d
Derek Morey <derek.o.morey@gmail.com>
2024-02-25 18:07:19 -0500

    Added ruff as dev dependency, removed some unused imports

--- songmanager/core/views.py
+++ songmanager/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.decorators.http import require_POST, require_GET, require_http_methods
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.http import HttpResponse
