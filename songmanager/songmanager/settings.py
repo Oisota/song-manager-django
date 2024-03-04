3db4bb865deabddeed1d5754623d8d8721204fcb
Derek Morey <derek.o.morey@gmail.com>
2024-03-03 21:57:10 -0500

    Started adding stuff for running in docker

--- songmanager/songmanager/settings.py
+++ songmanager/songmanager/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t0%69@fn8*b!2=p$*-&1_r9e2!8z0+u5f@awy)=55iaa#zl0b2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -34,7 +38,7 @@ INTERNAL_IPS = [
 ]
 
 # Logging
-LOG_LEVEL = 'DEBUG' if DEBUG else 'INFO'
+LOG_LEVEL = os.environ.get('LOG_LEVEL')
 
 LOGGING = {
     "version": 1,
@@ -121,8 +125,14 @@ WSGI_APPLICATION = 'songmanager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),   
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
